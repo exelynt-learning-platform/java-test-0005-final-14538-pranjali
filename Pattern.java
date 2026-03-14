d826520aeb9c4570233006333052c64cee762c91
Pranjali-Neware <pranjalineware20@gmail.com>
2026-03-14 15:35:58 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,9 +1,9 @@
 public class Pattern{
-  private int rows=5;
+  private static final int ROWS=5;
   public static void main(String []args)
   {
     int num=1;
-    for(int i=1;i<=rows;i++){
+    for(int i=1;i<=ROWS;i++){
       for(int j=1;j<=i;j++){
         System.out.print(num+" ");
         num++;
